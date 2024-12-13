2b6243fb32bafd5c4e9464aab972284d59a84eba
DavidMC12 <davidmadridcardozo@gmail.com>
2024-12-13 10:55:19 -0500

    Refactoring code

--- tests/signup.py
+++ tests/signup.py
@@ -59,9 +59,9 @@ def main(driver):
         fields = {
             "name": (SELECTORS["name_field"], "Usuario Prueba"),
             "last_name": (SELECTORS["last_name_field"], "Automatizado Prod"),
-            "email": (SELECTORS["email_field"], "u.aut8@mail.prod"),
+            "email": (SELECTORS["email_field"], "u.aut9@mail.prod"),
             "password": (SELECTORS["password_field"], "123456"),
-            "phone": (SELECTORS["phone_field"], "3214445558"),
+            "phone": (SELECTORS["phone_field"], "3214445559"),
         }
 
         for key, (selector, value) in fields.items():
