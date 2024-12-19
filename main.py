d6f058a0ab71ab4a5ed4aa481b2f4004593faff7
DavidMC12 <davidmadridcardozo@gmail.com>
2024-12-19 15:46:50 -0500

    Refactoring code

--- main.py
+++ main.py
@@ -3,27 +3,6 @@ import allure
 from setup import driver_setup as ds
 from tests import test_money_transfer_picash, test_picash_recharge, test_accept_driver_services, test_driver_register, test_bicitaxi_request, test_grua_request, test_chat_central, test_add_new_vehicle, service_moto_request, service_motovip_request, signup, service_pibox_request, programed_service_moto, test_login
 
-# Caso de prueba para pytest
-# @pytest.mark.allure
-# def test_login():
-#     # Inicializar el driver
-#     driver = ds.init_driver()
-
-#     # Ejecutar el proceso de login
-#     with allure.step("Ejecutar flujo de login"):
-#         test_login.test_login_flow(driver)
-
-#     # Finalizar driver después de las pruebas
-#     driver.quit()
-
-# Más casos de prueba pueden ir aquí
-# @pytest.mark.allure
-# def test_signup():
-#     driver = ds.init_driver()
-#     with allure.step("Ejecutar flujo de registro"):
-#         signup.signUp(driver)
-#     driver.quit()
-
 # Función principal, no para pytest, solo para ejecución manual
 def main():
     # Inicializar el driver
@@ -32,8 +11,8 @@ def main():
     # Ejecutar el proceso de login manualmente
     # test_login.test_login_flow(driver)
     # Ejecutar otros procesos comentados si es necesario
-    test_login.main(driver)
-    # signup.main(driver)
+    # test_login.main(driver)
+    signup.main(driver)
     # service_moto_request.booking_request(driver)
     # service_motovip_request.booking_request(driver)
     # service_pibox_request.main(driver)
